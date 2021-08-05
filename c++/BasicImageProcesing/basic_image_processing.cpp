88fef6c0574417c079843ce30bb96b4666f53bfb
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-08-06 00:04:52 +0530

    basic image processing added

---
+++ c++/BasicImageProcesing/basic_image_processing.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+int main(int argc, char ** argv)
+{
+    if(argc !=2)
+    {
+        printf("usage: ./BasicImageProcessing <ImageFilePath> \n");
+        return -1;
+    }
+
+    Mat image, gray, blur, canny;
+    Mat imgDia,imgErode;
+    image = imread(argv[1],1);
+    cvtColor(image,gray,COLOR_BGR2GRAY); // BRF to GRAY
+    GaussianBlur(image, blur,Size(3,3), 5,0);
+    Canny(blur,canny,25,75);
+    
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(5,5));
+    dilate(canny,imgDia,kernel);
+    erode(imgDia,imgErode,kernel);
+
+    Mat resized;
+    resize(image,resized, Size(1000,300));
+
+    Rect roi(60,120,120,50);
+
+    Mat cropped = image(roi);
+    imshow("original",image);
+    imshow("Gray",gray);
+    imshow("Blur",blur);
+    imshow("Canny",canny);    
+    imshow("Dilated",imgDia);
+    imshow("Erode",imgErode);
+    imshow("Resized",resized);
+    imshow("Cropped",cropped);
+
+    waitKey(0);
+    return 0;
+}
