276c41f03fc7cf3f2d6cf7ac6dead0059c71ef68
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-08-05 16:10:17 +0530

    description added

--- c++/Changing Brightness/change_brightness_of_image.cpp
+++ c++/Changing Brightness/change_brightness_of_image.cpp
@@ -26,6 +26,26 @@ int main(int argc, char** argv)
     image.convertTo(image_brightness_m_50, -1, 1, -50);
     image.convertTo(image_brightness_m_100, -1, 1, -100);
 
+    /*
+    convertTo()
+    
+    void cv::Mat::convertTo(OutputArray m,int rtype, double alpha = 1, double beta = 0)const
+    
+    Converts an array to another data type with optional scaling.
+
+    The method converts source pixel values to the target data type. saturate_cast<> is applied at the end to avoid possible overflows:
+    m(x,y)=saturate_cast<rType>(α(∗this)(x,y)+β)
+    
+    Parameters
+    m:	output matrix; if it does not have a proper size or type before the operation, it is reallocated.
+    rtype:	desired output matrix type or, rather, the depth since the number of channels are the same as the input has; if rtype is negative, the output matrix will have the same type as the input.
+    alpha:	optional scale factor.
+    beta:	optional delta added to the scaled values.
+    */
+
+
+
+
     namedWindow("Original",WINDOW_AUTOSIZE);
     namedWindow("Increased 50% brightness",WINDOW_AUTOSIZE);
     namedWindow("Increased 100% brightness",WINDOW_AUTOSIZE);
