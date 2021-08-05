4085429042bfbaea741a7314fd749422f52fcd98
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-08-06 03:47:34 +0530

    error handling

--- c++/BasicImageProcesing/basic_image_processing.cpp
+++ c++/BasicImageProcesing/basic_image_processing.cpp
@@ -7,7 +7,7 @@ int main(int argc, char ** argv)
     if(argc !=2)
     {
         printf("usage: ./BasicImageProcessing <ImageFilePath> \n");
-        return -1;
+        return 1;
     }
 
     Mat image, gray, blur, canny;
