67cf582e91908e26cce57688cfd57a7ff4ac440b
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-08-06 03:48:33 +0530

    error handling

--- c++/Loading an Image/load_an_image.cpp
+++ c++/Loading an Image/load_an_image.cpp
@@ -7,14 +7,14 @@ int main(int argc, char** argv )
     if ( argc != 2 )
     {
         printf("usage: DisplayVideo.out <Image_Path>\n");
-        return -1;
+        return 1;
     }
     Mat image;
     image = imread( argv[1], 1 );
     if ( !image.data )
     {
         printf("No image data \n");
-        return -1;
+        return 1;
     }
     namedWindow("Display Image", WINDOW_AUTOSIZE ); //this is optional if you want to tinker with the window size
      
